ae6f8da49597b1e0e4cbbcd93d0a71d24636b8ca
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-10-15 17:43:48 -0500

    added map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import UserProfile from './components/user-profile/user-profile.component';
-import UserList from './components/user-list/user-list.component';
+import UserProfile from "./components/user-profile/user-profile.component";
+import UserList from "./components/user-list/user-list.component";
 
-import './App.scss';
+import "./App.scss";
 
-function App() {
-  return (
-    <div className='App'>
-      <UserList dataSource='https://jsonplaceholder.typicode.com/users' />
-      <UserProfile
-        name='Yihua'
-        email='yihuazhang@gmail.com'
-        dataSource='https://jsonplaceholder.typicode.com/posts'
-      />
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        this.setState({ posts: data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.email}</h2>
+
+        <div>
+          <h2>Posts: </h2>
+          {this.props.data.map((post) => {
+            <div className="post" key={post.id}>
+              <h2>{post.title}</h2>
+            </div>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
